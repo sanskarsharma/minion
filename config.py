9816e3415711c1551ea29cce716d5e4ae5e129a7
sanskarsharma <sanskar2996@gmail.com>
2018-08-14 07:29:03 +0000

    pyaw commit - added dotenv config, live on pyaw

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-can-easily-guess'
 
+    MYSQL_DB_HOST = os.environ.get("MYSQL_DB_HOST")
+    MYSQL_DB_NAME = os.environ.get("MYSQL_DB_NAME")
+
     MYSQL_DB_USERNAME = os.environ.get("MYSQL_DB_USERNAME")
     MYSQL_DB_PASSWORD = os.environ.get("MYSQL_DB_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + MYSQL_DB_USERNAME + ":" + MYSQL_DB_PASSWORD + "@localhost/urlshortener"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + MYSQL_DB_USERNAME + ":" + MYSQL_DB_PASSWORD + "@"+ MYSQL_DB_HOST +"/" + MYSQL_DB_NAME
+
+#    MYSQL_DB_USERNAME = os.environ.get("MYSQL_DB_USERNAME")
+#    MYSQL_DB_PASSWORD = os.environ.get("MYSQL_DB_PASSWORD")
+#    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + MYSQL_DB_USERNAME + ":" + MYSQL_DB_PASSWORD + "@localhost/urlshortener"
     
     
 
@@ -15,4 +25,4 @@ class Config(object):
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DOMAIN_NAME= "sanskarsharma.tech"
+    DOMAIN_NAME= os.environ.get("DOMAIN_NAME")
